de906ba8ca44cbe8c9ed21df21683dd9fb231bcc
Alex Karobia <akarobia30@gmail.com>
2024-06-11 11:06:15 +0300

    theme modifiation

--- src/componets/Model.jsx
+++ src/componets/Model.jsx
@@ -37,8 +37,8 @@ const Model = () => {
       <div className={` flex w-full rounded-3xl items-center justify-center min-h-screen p-4 overflow-hidden ${theme === "dark" ?  "bg-transparent text-white" : "bg-white text-black"}`}>
       
         <div className="relative flex-col lg:flex-center items-center justify-center  rounded-3xl p-4 w-full md:w-full lg:w-full ">
+          <h1>Theme</h1>
           <button onClick={toggleTheme} className="px-5 py-2 rounded-3xl bg-blue-950 my-5 h border border-transparent hover:border hover:text-white hover:border-blue-950 transition-transform  md:mx-auto lg:ml-auto lg:mr-0  md:mr-5 sm:text-left sm:mr-0 sm:mx-auto">
-               <h1>Theme</h1>
               {theme === "dark" ? <img src={lightImg}
               style={{ width: '24px', height: '24px', filter: 'invert(1)' }} /> 
               : <img src={darkImg}
