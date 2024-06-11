2c2ad435384cb0228dcfc1200f2b57fe55757588
Alex Karobia <akarobia30@gmail.com>
2024-06-11 10:57:35 +0300

    theme modifiation

--- src/componets/Model.jsx
+++ src/componets/Model.jsx
@@ -33,16 +33,16 @@ const Model = () => {
 
   return (
     <section className="flex-center flex-col ">
-    <h1 className="text-center font-semibold lg:text-8xl md:text-7xl text-5xl text-white  max-md:mb-10">This is the full image and its Documentation</h1>
+    <h1 className="text-center font-semibold lg:text-4xl md:text-3xl sm:text-2xl text-white  max-md:mb-10">More informtion</h1>
       <div className={` flex w-full rounded-3xl items-center justify-center min-h-screen p-4 overflow-hidden ${theme === "dark" ?  "bg-transparent text-white" : "bg-white text-black"}`}>
       
         <div className="relative flex-col lg:flex-center items-center justify-center  rounded-3xl p-4 w-full md:w-full lg:w-full ">
           <button onClick={toggleTheme} className="px-5 py-2 rounded-3xl bg-blue-950 my-5 h border border-transparent hover:border hover:text-white hover:border-blue-950 transition-transform  md:mx-auto lg:ml-auto lg:mr-0  md:mr-5 sm:text-left sm:mr-0 sm:mx-auto">
-               
+               <h1>Theme</h1>
               {theme === "dark" ? <img src={lightImg}
-              style={{ width: '36px', height: '36px', filter: 'invert(1)' }} /> 
+              style={{ width: '24px', height: '24px', filter: 'invert(1)' }} /> 
               : <img src={darkImg}
-              style={{ width: '28px', height: '28px', filter: 'invert(1)' }}/>}
+              style={{ width: '24px', height: '24px', filter: 'invert(1)' }}/>}
           </button>
           <img
             id="imgAnimation"
