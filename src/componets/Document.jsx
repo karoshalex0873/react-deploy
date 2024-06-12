f69043d134920c6c03fd7938bee6e2f29f368677
Alex Karobia <akarobia30@gmail.com>
2024-06-12 11:29:31 +0300

    split animation

--- src/componets/Document.jsx
+++ src/componets/Document.jsx
@@ -56,23 +56,33 @@ const Document = () => {
                 {invention.title}
               </motion.h2>
               <p className="text-lg text-gray-300 mb-4">{invention.description}</p>
-              <div className="flex space-x-2">
-                <motion.img
-                  src={invention.image}
-                  alt={invention.title}
-                  className="w-1/2 rounded-lg"
+              <div className="flex space-x-2 justify-center">
+                <motion.div
+                  className="w-1/2"
                   initial={{ opacity: 0, x: -50 }}
                   whileInView={{ opacity: 1, x: 0 }}
                   transition={{ duration: 0.5 }}
-                />
-                <motion.img
-                  src={invention.image}
-                  alt={invention.title}
-                  className="w-1/2 rounded-lg"
+                >
+                  <img
+                    src={invention.image}
+                    alt={invention.title}
+                    className="w-full h-auto rounded-lg"
+                    style={{ clipPath: 'inset(0 50% 0 0)' }}
+                  />
+                </motion.div>
+                <motion.div
+                  className="w-1/2"
                   initial={{ opacity: 0, x: 50 }}
                   whileInView={{ opacity: 1, x: 0 }}
                   transition={{ duration: 0.5 }}
-                />
+                >
+                  <img
+                    src={invention.image}
+                    alt={invention.title}
+                    className="w-full h-auto rounded-lg"
+                    style={{ clipPath: 'inset(0 0 0 50%)' }}
+                  />
+                </motion.div>
               </div>
             </motion.div>
           </Step>
